128bfea0522c3f79006b3042b20ffd19d61870fa
Matan <skrjzor@gmail.com>
2020-07-18 10:35:28 +0300

    v1.2 localAPI

--- src/redux/asyncActions.js
+++ src/redux/asyncActions.js
@@ -16,12 +16,18 @@ import axios from "axios";
 export const getCurrentCityData = (cityKey) => {
   return async (dispatch) => {
     dispatch(fetchStarted());
+    // setTimeout(async () => {
     try {
-      const currentResponse = await axios.get(
-        `https://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${process.env.REACT_APP_API_KEY}&metric=true`
-      );
+      // const currentResponse = await axios.get(
+      //   `http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${process.env.REACT_APP_API_KEY}&metric=true`
+      // );
+      // const forecastResponse = await axios.get(
+      //   `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${process.env.REACT_APP_API_KEY}&metric=true`
+      // );
+
+      const currentResponse = await axios.get("/localapi/telavivCurrent.json");
       const forecastResponse = await axios.get(
-        `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${cityKey}?apikey=${process.env.REACT_APP_API_KEY}&metric=true`
+        "/localapi/telavivForecast.json"
       );
 
       dispatch(setCurrentWeather(currentResponse.data[0]));
@@ -30,6 +36,7 @@ export const getCurrentCityData = (cityKey) => {
     } catch (err) {
       dispatch(fetchFailed());
     }
+    // }, 1000);
   };
 };
 
@@ -37,16 +44,20 @@ export const searchCity = (cityName) => {
   return async (dispatch) => {
     dispatch(fetchStarted());
 
+    // setTimeout(async () => {
     try {
-      const searchResponse = await axios.get(
-        `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${process.env.REACT_APP_API_KEY}&q=${cityName}`
-      );
+      // const searchResponse = await axios.get(
+      //   `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${process.env.REACT_APP_API_KEY}&q=${cityName}`
+      // );
+
+      const searchResponse = await axios.get("/localapi/search.json");
 
       dispatch(setSearchResults(searchResponse.data));
       dispatch(fetchSuccess());
     } catch (err) {
       dispatch(fetchFailed());
     }
+    // }, 1000);
   };
 };
 
@@ -54,16 +65,20 @@ export const getCurrentFavoriteCityData = (cityKey) => {
   return async (dispatch) => {
     dispatch(fetchFavoriteStarted(cityKey));
 
+    // setTimeout(async () => {
     try {
-      const currentResponse = await axios.get(
-        `https://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${process.env.REACT_APP_API_KEY}&metric=true`
-      );
+      // const currentResponse = await axios.get(
+      //   `http://dataservice.accuweather.com/currentconditions/v1/${cityKey}?apikey=${process.env.REACT_APP_API_KEY}&metric=true`
+      // );
+
+      const currentResponse = await axios.get("/localapi/telavivCurrent.json");
 
       dispatch(setFavoriteCurrentWeather(cityKey, currentResponse.data[0]));
       dispatch(fetchFavoriteSuccess(cityKey));
     } catch (err) {
       dispatch(fetchFavoriteFailed(cityKey));
     }
+    // }, 1000);
   };
 };
 
@@ -76,8 +91,12 @@ export const getCurrentLocation = () => {
         try {
           const { latitude, longitude } = result.coords;
 
+          // const locationResponse = await axios.get(
+          //   `http://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${process.env.REACT_APP_API_KEY}&q=${latitude},${longitude}`
+          // );
+
           const locationResponse = await axios.get(
-            `https://dataservice.accuweather.com/locations/v1/cities/geoposition/search?apikey=${process.env.REACT_APP_API_KEY}&q=${latitude},${longitude}`
+            "/localapi/currentLocation.json"
           );
 
           const cityData = {
